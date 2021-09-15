db8c3dbad35d7747db13aedf33b83ff5f8671d7e
Clarence <clarencelindh@gmail.com>
2021-09-15 11:52:24 +0000

    Moved to CLI

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,40 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
+
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-];
+ {
+  path: "/",
+  name: "Home",
+  component: Home,
+  meta: {
+   title: 'Home'
+  }
+ },
+ {
+  path: "/About",
+  name: "About",
+  component: About,
+  meta: {
+   title: 'About'
+  }
+ },
+
+]
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+ mode: "history",
+ base: process.env.BASE_URL,
+ routes,
 });
+
+router.beforeEach((to, from, next) => {
+ document.title = `${to.meta.title} | Blog`;
+ next();
+})
 
 export default router;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,9 @@
 <template>
-  <h1>Home</h1>
-  
+  <h1>About</h1>
 </template>
 
 <script>
-
-
 export default {
- name: "Home",
-
- components: {
-
- }
 
 }
 </script>
